2b849c59a3fdb3fe9277163da3186cf5e27cab69
bravoseb <bravoseb@shared-qc3503.Corp.FC.LOCAL>
2015-11-13 16:15:49 -0500

    Actualizacion de security.xml, pendiente validar autenticacion

--- coopweb/src/main/java/ca/cooperative/controller/inventario/ProductController.java
+++ coopweb/src/main/java/ca/cooperative/controller/inventario/ProductController.java
@@ -3,10 +3,16 @@ package ca.cooperative.controller.inventario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ca.cooperative.inventario.model.Product;
 import ca.cooperative.inventario.service.ProductService;
 
 @Controller
@@ -18,21 +24,38 @@ public class ProductController {
 	@RequestMapping 
 	public String list(Model model) {
 		model.addAttribute("products", productService.getAllProducts());
-		return "products";
+		return "product/products";
 	}
 	
 	@RequestMapping("/all")
 	public ModelAndView allProducts() {
 		ModelAndView modelAndView = new ModelAndView();		
 		modelAndView.addObject("products", productService.getAllProducts());
-		modelAndView.setViewName("products");
+		modelAndView.setViewName("product/products");
 		return modelAndView;
 	}
 	
 	@RequestMapping("/product")
 	public String getProductById(@RequestParam("codeProduct") String codeProduct, Model model) {
 		model.addAttribute("product", productService.getProductByCode(codeProduct));
-		return "product";
+		return "product/product";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String getAddNewProductForm(@ModelAttribute("newProduct") Product newProduct) {
+	   return "product/addProduct";
+	}
+	   
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String processAddNewProductForm(@ModelAttribute("newProduct") Product productToBeAdded, ModelMap map, BindingResult result) {
+		String[] suppressedFields = result.getSuppressedFields();
+		
+		if (suppressedFields.length > 0) {
+			throw new RuntimeException("Attempting to bind disallowed fields: " + StringUtils.arrayToCommaDelimitedString(suppressedFields));
+		}
+		
+	   	productService.addProduct(productToBeAdded);
+		return "redirect:product/products";
 	}
 
 }
